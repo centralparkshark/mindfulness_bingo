6694c2aac6b43005d48988fc7425a1651087a7e9
Vinncent Miller <vinncentmiller@gmail.com>
2024-08-18 08:15:25 -0400

    Refactored to include document fragment

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 //import file from "./content.json" with { type: "json"}
 let activities = []; 
 
-async function getData() {
-    fetch("./content.json")
-    .then((res) => res.json())
-    .then((data) => {activities.push(data);
-        generateBoard(data);
-    }) 
-}
+// async function getData() {
+//     fetch("./content.json")
+//     .then((res) => res.json())
+//     .then((data) => {activities.push(data);
+//         generateBoard(data);
+//     }) 
+// }
 
-getData();
+// getData();
 
 const bingoCard = document.getElementById("bingoCard")
 const popupCtn = document.querySelector("#popupCtn")
 const popup = document.getElementById("popup")
 
 function generateBoard(items) {
+    const fragment = new DocumentFragment();
     items.forEach(element => {
         const bingoSpot = document.createElement("div")
         bingoSpot.setAttribute( "id", element.id );
@@ -28,14 +29,15 @@ function generateBoard(items) {
             <div class="icon"><i class="fa fa-${element.icon}"></i></div>
         `
         bingoSpot.addEventListener('click', openPopup)
-        bingoCard.appendChild(bingoSpot)
+        fragment.appendChild(bingoSpot)
     });
+    bingoCard.appendChild(fragment)
 }
 
 
 
 function openPopup() {
-    let obj = (activities[0][this.id])
+    let obj = (activities[this.id])
     popupCtn.style.display = "flex";
     popup.innerHTML = `<h1 class="title">${obj.title}</h1>
             <div class="info">${obj.info}</div>
@@ -48,10 +50,10 @@ function openPopup() {
     toggleStatusBtn.addEventListener('click', () => {
         obj.done = !obj.done;
         closePopup();
-
         bingoCard.childNodes[this.id].classList.toggle("done");
     })
 }
+
 
 function closePopup() {
     popupCtn.style.display = "none"
@@ -71,13 +73,21 @@ function clearBoard() {
 }
 
 //To-Do:
-// Make reset button work (settings screen?)
-    // confirm: are you sure you'd like to reset
-// Include at least one form and/or input with HTML attribute validation.
+
 // also event based validation on input
 // Use at least two Browser Object Model (BOM) properties or methods.
 // Add local storage
 // Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
-// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 
 // Modify at least one attribute of an element in response to user interaction.
 // Read me
+
+
+activities = [
+    {"id": 0, "title": "Box Breathing", "info": "testing", "icon": "face-smile", "done": false},
+    {"id": 1, "title": "Gratitude", "info": "<form id='gratitude'><label for='gratitude'>What is one thing you are grateful for?</label><input type='text' name='gratitude' id='gratitude' placeholder='I am grateful for...' required minLength='2'></form>", "icon": "face-smile", "done": false},
+    {"id": 2, "title": "Box Breathing", "info": "testing", "icon": "face-smile", "done": false},
+    {"id": 3, "title": "Box Breathing", "info": "testing", "icon": "face-smile", "done": false},
+    {"id": 4, "title": "Box Breathing", "info": "testing", "icon": "face-smile", "done": false}
+]
+
+generateBoard(activities)
